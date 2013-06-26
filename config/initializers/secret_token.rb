3294d7351be2817d67324c3807966727e1b55018
Aaron Dufall <aald212@gmail.com>
2013-06-26 10:52:18 -0500

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaddyKnows::Application.config.secret_token = 'bb272d8a9aa6c279c5957c6d966940ef1f55a9f6e582df6f42be69ed88dca9cc755c06b919d6302b7b4e04f9973d1615af4158e476f8cbdbd1127bdeb911ab48'
