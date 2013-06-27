9b7e4c485f2286305acda4005c14b068eee6f757
rodlevy <rodlevy1@gmail.com>
2013-06-27 11:22:53 -0500

    WIP: capybara user error

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe "the signup process" do
-  # before :each do
-  #   User.make(:email => 'rod@rod.com', :password => 'rodrodrod')
-  # end
+  before :each do
+  #   User.create(:email => 'rod@rod.com', :password => 'rodrodrod')
+    visit new_user_registration_path
+  end
 
-  it "signs me in" do
-    visit '/users/sign_up'
-    within("#session") do
+  it "signs me up" do
+    # within("#session") do
       fill_in 'Email', :with => 'rod@rod.com'
       fill_in 'user_password', :with => 'rodrodrod'
       fill_in 'user_password_confirmation', :with => 'rodrodrod'
-    end
+    # end
     click_link 'Sign Up'
     expect(page).to have_content 'Success'
   end
