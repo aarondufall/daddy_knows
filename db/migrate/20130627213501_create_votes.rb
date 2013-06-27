c2a7083e67f35856b1366eef2de7c50acd5af49a
Nestor Castillo <nes_23@yahoo.com>
2013-06-27 16:53:42 -0500

    Fixed error in migration

--- db/migrate/20130627213501_create_votes.rb
+++ db/migrate/20130627213501_create_votes.rb
@@ -1,6 +1,6 @@
 class CreateVotes < ActiveRecord::Migration
  def change
-  create_table :votes do |to|
+  create_table :votes do |t|
     t.integer :user_id
     t.belongs_to :votable, polymorphic: true
   end
