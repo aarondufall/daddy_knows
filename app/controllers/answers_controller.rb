58797c3ae9cb7b93f186381b64750d0444700a03
Aaron Dufall <aald212@gmail.com>
2013-06-28 10:47:26 -0500

    Added answers controller with create action

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,11 @@
+class AnswersController < ApplicationController
+	def create
+		@question = Question.find(params[:question_id])
+		@answer = @question.answers.build(params[:answer])
+		if @answer.save
+			redirect_to @question, notice: "Answer succesfully submitted"
+		else
+			redirect_to @question, notice: "Failed to save answer"
+		end
+	end
+end
