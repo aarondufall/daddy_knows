e7669bab8347e7a661b44778f6ed138f923b4de1
Aaron Dufall <aald212@gmail.com>
2013-06-28 14:37:01 -0500

    Token JS

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	 bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
+
+  jQuery(function() {
+    return $('#question_tag_tokens').tokenInput('/tags.json', {
+      // prePopulate: tagData || null
+    });
+  });
+
 });
